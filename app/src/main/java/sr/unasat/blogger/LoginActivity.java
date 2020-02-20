ce24ccbb4ff583e3bfcaecf2cdf106a752609b5f
sitaict <shayant.sital@telesur.sr>
2020-02-20 08:06:25 -0300

    added delay to next screen(login)

---
+++ app/src/main/java/sr/unasat/blogger/LoginActivity.java
@@ -0,0 +1,14 @@
+package sr.unasat.blogger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/sr/unasat/blogger/MainActivity.java
+++ app/src/main/java/sr/unasat/blogger/MainActivity.java
@@ -2,7 +2,9 @@ package sr.unasat.blogger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -10,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_DURATION = 5000;
 
     //Animations
     Animation topAnimation, bottemAnimation;
@@ -38,5 +42,14 @@ public class MainActivity extends AppCompatActivity {
         logo.setAnimation(topAnimation);
         name.setAnimation(bottemAnimation);
         slogan.setAnimation(bottemAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); //zodat de gebruiker niet terug kan gaan (verwijderd het van de act list)
+            }
+        }, SPLASH_SCREEN_DURATION);
     }
 }
