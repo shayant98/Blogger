bcf90a0125ad8227b6f001e67194d98d75e64525
sitaict <shayant.sital@telesur.sr>
2020-02-20 07:58:26 -0300

    added animations added splashscreen

--- app/src/main/java/sr/unasat/blogger/MainActivity.java
+++ app/src/main/java/sr/unasat/blogger/MainActivity.java
@@ -3,12 +3,40 @@ package sr.unasat.blogger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Animations
+    Animation topAnimation, bottemAnimation;
+    ImageView logo;
+    TextView name, slogan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); //set app als fullscreen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //loadAnimtions
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_top_animation);
+        bottemAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_bottom_animation);
+
+
+        //get fields
+        logo = findViewById(R.id.logo);
+        name = findViewById(R.id.name);
+        slogan = findViewById(R.id.slogan);
+
+
+        //set animation
+        logo.setAnimation(topAnimation);
+        name.setAnimation(bottemAnimation);
+        slogan.setAnimation(bottemAnimation);
     }
 }
