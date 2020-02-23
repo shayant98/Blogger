2c674d2cc2e71948f77454608f1c06de7d0a30e4
Shayant Sital <shayant@gmail.com>
2020-02-23 20:50:20 -0300

    modified user class modified databasehelper

--- app/src/main/java/sr/unasat/blogger/Entity/User.java
+++ app/src/main/java/sr/unasat/blogger/Entity/User.java
@@ -1,4 +1,72 @@
 package sr.unasat.blogger.Entity;
 
 public class User {
+
+    private String id;
+    private String email;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String id, String email, String username, String password, String role) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+
+
 }
--- app/src/main/java/sr/unasat/blogger/database/DatabaseHelper.java
+++ app/src/main/java/sr/unasat/blogger/database/DatabaseHelper.java
@@ -1,11 +1,13 @@
 package sr.unasat.blogger.database;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import sr.unasat.blogger.Entity.User;
 import sr.unasat.blogger.database.userContract;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -22,10 +24,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         final String SQL_CREATE_USER_TABLE = " CREATE TABLE " + userContract.UserEntry.TABLE_NAME +
                 " (" +
-                userContract.UserEntry.USERS_ID + " INTEGER PRIMARY KEY," +
+                userContract.UserEntry.USERS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 userContract.UserEntry.USERS_PERSON_ID + " INTEGER NOT NULL," +
                 " FOREIGN KEY(" + userContract.UserEntry.USERS_PERSON_ID + ") REFERENCES" + "Hier_moet foreign table name en (PK)"
-                + userContract.UserEntry.USERS_USERNAME + " STRING NOT NULL UNIQUE," + userContract.UserEntry.USERS_EMAIL + " STRING NOT NULL UNIQUE," + userContract.UserEntry.USERS_PASSWORD + " STRING NOT NULL," + userContract.UserEntry.USERS_BIO + " TEXT," + userContract.UserEntry.USERS_ROLE + " STRING NOT NULL," + userContract.UserEntry.USERS_DATE_CREATED + "TEXT," + userContract.UserEntry.USERS_DATE_UPDATED + " TEXT)";
+                + userContract.UserEntry.USERS_USERNAME + " STRING NOT NULL UNIQUE," +
+                userContract.UserEntry.USERS_EMAIL + " STRING NOT NULL UNIQUE,"
+                + userContract.UserEntry.USERS_PASSWORD + " STRING NOT NULL,"
+                + userContract.UserEntry.USERS_BIO + " TEXT,"
+                + userContract.UserEntry.USERS_ROLE + " STRING NOT NULL,"
+                + userContract.UserEntry.USERS_DATE_CREATED + "TEXT,"
+                + userContract.UserEntry.USERS_DATE_UPDATED + " TEXT)";
 
 
 
@@ -35,4 +43,5 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
 }
