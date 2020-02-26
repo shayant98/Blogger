ced03a18658f94fd6a979223c0a7ff50b8ce1098
Shayant Sital <shayant@gmail.com>
2020-02-25 21:51:02 -0300

    removed unused deps moved toolbar to a appbar layout for collapse functionality
    changed scrollview in account to nestedScrollView for collpasing appbar
    
    changed menu titles
    added strings for menu titles

--- app/src/main/java/sr/unasat/blogger/FeedFragment.java
+++ app/src/main/java/sr/unasat/blogger/FeedFragment.java
@@ -1,7 +1,6 @@
 package sr.unasat.blogger;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
@@ -10,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 
 public class FeedFragment extends Fragment {
@@ -24,9 +25,13 @@ public class FeedFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
+
+
         toolbar = getActivity().findViewById(R.id.toolbar);
 
+
         toolbar.setBackgroundColor(getResources().getColor(R.color.textColorDark));
+
 
         return inflater.inflate(R.layout.fragment_feed, container, false);
     }
--- app/src/main/java/sr/unasat/blogger/NavigationActivity.java
+++ app/src/main/java/sr/unasat/blogger/NavigationActivity.java
@@ -10,7 +10,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
